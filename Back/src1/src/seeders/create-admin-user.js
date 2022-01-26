d7261b09a90fd79cf04b99b3c492a8bb27b21bbf
chicano85 <chicano.cano@gmail.com>
2022-01-26 15:18:42 +0100

    feat: add script to create admin

--- Back/src1/src/seeders/create-admin-user.js
+++ Back/src1/src/seeders/create-admin-user.js
@@ -1,4 +1,44 @@
+// const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const uuid = require('uuid');
+const db = require('../config/db');
+
+const { User, UserGroup } = require('../models/index');
+
+// connection with database
+db.connect();
+
+const password = bcrypt.hashSync('123456', bcrypt.genSaltSync(10));
+
+/*     if (superAdminGroup.length > 0) {
+      superAdminGroup = superAdminGroup.pop(); */
+
+const seedDB = async () => {
+  const adminGroup = await UserGroup.findOne({ name: 'Admin' });
+  console.log(adminGroup);
+  if (!adminGroup) {
+    throw new Error('no se ha encontrado');
+  }
+
+  const seedAdminShema = {
+    uuid: uuid.v4(),
+    name: 'Administrador',
+    email: 'admin@admin.com',
+    password,
+    role_uuid: adminGroup.uuid,
+    token: '',
+    active: true,
+  };
+  await User.deleteMany({});
+  await User.insertMany(seedAdminShema);
+};
+
+// Seed
+seedDB().then(() => {
+  db.disconnect();
+});
+
+/* const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 const { UserGroup } = require('../models');
 
@@ -42,3 +82,4 @@ module.exports = {
     await queryInterface.bulkDelete('user_groups', null, {});
   },
 };
+ */
