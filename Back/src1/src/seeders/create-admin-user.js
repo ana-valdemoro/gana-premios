034c75d0744086cd33a2f48df4f417dfdff167d1
chicano85 <chicano.cano@gmail.com>
2022-01-26 15:44:59 +0100

    delete comments on create-admin

--- Back/src1/src/seeders/create-admin-user.js
+++ Back/src1/src/seeders/create-admin-user.js
@@ -10,9 +10,6 @@ db.connect();
 
 const password = bcrypt.hashSync('123456', bcrypt.genSaltSync(10));
 
-/*     if (superAdminGroup.length > 0) {
-      superAdminGroup = superAdminGroup.pop(); */
-
 const seedDB = async () => {
   const adminGroup = await UserGroup.findOne({ name: 'Admin' });
   console.log(adminGroup);
@@ -37,49 +34,3 @@ const seedDB = async () => {
 seedDB().then(() => {
   db.disconnect();
 });
-
-/* const uuid = require('uuid');
-const bcrypt = require('bcrypt');
-const { UserGroup } = require('../models');
-
-module.exports = {
-  up: async (queryInterface) => {
-    try {
-      await queryInterface.bulkInsert('user_groups', [
-        {
-          uuid: uuid.v4(),
-          name: 'Superadmin',
-          permissions: 'SUPERADMIN',
-        },
-      ]);
-
-      let superAdminGroup = await UserGroup.findAll({ where: { name: 'Superadmin' } });
-
-      if (superAdminGroup.length > 0) {
-        superAdminGroup = superAdminGroup.pop();
-      }
-
-      const password = bcrypt.hashSync('123456', bcrypt.genSaltSync(10));
-
-      await queryInterface.bulkInsert('users', [
-        {
-          uuid: uuid.v4(),
-          name: 'Administrador',
-          email: 'admin@admin.com',
-          password,
-          role_uuid: superAdminGroup.uuid,
-          token: '',
-          active: true,
-        },
-      ]);
-    } catch (e) {
-      console.log(e);
-    }
-  },
-
-  down: async (queryInterface) => {
-    await queryInterface.bulkDelete('users', null, {});
-    await queryInterface.bulkDelete('user_groups', null, {});
-  },
-};
- */
