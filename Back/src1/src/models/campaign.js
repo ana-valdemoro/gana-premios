2588187a31479afa2c0c1f49c871c8eb8810e0d4
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-01-27 13:50:34 +0000

    FTR: define campaign model

---
+++ Back/src1/src/models/campaign.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    uuid: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    customer_uuid: {
+      required: true,
+      type: String,
+    },
+    manager_uuid: {
+      required: true,
+      type: String,
+    },
+    active: {
+      required: true,
+      type: Boolean,
+      default: false,
+    },
+    deleted: {
+      required: true,
+      type: Boolean,
+      default: false,
+    },
+    start_date: {
+      required: true,
+      type: Date,
+    },
+    end_date: {
+      required: true,
+      type: Date,
+    },
+    promotions: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret._id;
+      },
+    },
+  },
+);
+
+module.exports = schema;
