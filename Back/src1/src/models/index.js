b15476c8804408f5552c0b650e1df46d6f455090
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-01-27 16:11:49 +0000

    WIP: added campaign model to index

--- Back/src1/src/models/index.js
+++ Back/src1/src/models/index.js
@@ -1,24 +1,14 @@
 const mongoose = require('mongoose');
-// const Sequelize = require('../config/db');
-
-// const Activity = require('./activity');
 const UserSchema = require('./user');
 const UserGroupSchema = require('./userGroup');
+const CampaignSchema = require('./campaign');
 
 const models = {
   //   Activity: Activity.init(Sequelize, sequelize),
   User: mongoose.model('User', UserSchema),
   UserGroup: mongoose.model('UserGroups', UserGroupSchema),
+  Campaign: mongoose.model('Campaign', CampaignSchema),
 };
-
-// Object.values(models)
-//   .filter((model) => typeof model.associate === 'function')
-//   .forEach((model) => model.associate(models));
-
-// const db = {
-//   ...models,
-//   Sequelize,
-// };
 
 // We export the models variable to be used in App.
 module.exports = models;
