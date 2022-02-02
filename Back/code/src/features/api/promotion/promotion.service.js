8cf4484b75034866e52bf7ab57893c533057d7c4
chicano85 <chicano.cano@gmail.com>
2022-02-02 11:46:06 +0100

    WIP: add promotion.service, promotion.filters and promotion.middleware

--- Back/code/src/features/api/promotion/promotion.service.js
+++ Back/code/src/features/api/promotion/promotion.service.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line no-unused-vars
 const { v4: uuidv4 } = require('uuid');
 const { Promotion } = require('../../../models/index');
 
@@ -8,19 +10,33 @@ const DESERTED = 3;
 
 const toPublic = (promotion) => promotion.toJSON();
 
+const getPromotions = async (filters, options) =>
+  Promotion.find({ where: filters, promotion: options.promotion });
+
+const getPromotion = async (uuid) => Promotion.findOne({ uuid });
+
 const createPromotion = async (data) => {
-  const dataToCreate = {
-    ...data,
-    uuid: uuidv4(),
-  };
+  const dataToCreate = { ...data, uuid: uuidv4() };
   return Promotion.create(dataToCreate);
+};
+const putPromotion = async (uuid, data) => Promotion.findOneAndUpdate(uuid, data, { new: true });
+
+const deletePromotion = async (promotion) => promotion.remove();
+
+module.exports = {
+  toPublic,
+  getPromotions,
+  getPromotion,
+  createPromotion,
+  putPromotion,
+  deletePromotion,
+  DRAFT,
+  PUBLISHED,
+  FINISHED,
+  DESERTED,
 };
 
 module.exports = {
   toPublic,
   createPromotion,
-  DRAFT,
-  PUBLISHED,
-  FINISHED,
-  DESERTED,
 };
