ee83bd85905fb201d0c94c193e632fbfe89435d1
chicano85 <chicano.cano@gmail.com>
2022-02-02 11:36:04 +0100

    WIP: add promotion.controller

--- Back/code/src/features/api/promotion/promotion.controller.js
+++ Back/code/src/features/api/promotion/promotion.controller.js
@@ -1,92 +1,135 @@
-/* eslint-disable no-useless-escape */
+/* eslint-disable import/no-unresolved */
 const boom = require('@hapi/boom');
-const userService = require('./promotion.service');
+const { cloneDeep } = require('lodash');
+
+const { UniqueConstraintError } = require('sequelize');
+
+const promotionService = require('./promotion.service');
+const activityService = require('../activity/activity.service');
+const activityActions = require('./promotion.activity');
+const queryOptions = require('../../../utils/queryOptions');
+const promotionFilters = require('./promotion.filters');
 const logger = require('../../../config/winston');
 
-const createPromotion = async (req, res, next) => {
+const listPromotions = async (req, res, next) => {
   try {
-    if (res.locals && res.locals.user) {
-      return res.json(await userService.toPublic(res.locals.user));
-    }
-    res.json(await userService.toPublic(req.user));
+    const filters = promotionFilters(req.query);
+    const options = queryOptions(req.query);
+
+    res.json(await promotionService.getPromotions(filters, options));
   } catch (error) {
     logger.error(`${error}`);
     return next(boom.badImplementation(error.message));
   }
 };
 
-// const putUser = async (req, res, next) => {
-//   let { user } = req;
-//   if (res.locals && res.locals.user) {
-//     // eslint-disable-next-line prefer-destructuring
-//     user = res.locals.user;
-//   }
+const getPromotion = async (req, res, next) => {
+  const { promotionUuid } = req.params;
+  console.log(promotionUuid);
+  try {
+    const promotion = await promotionService.getClient(promotionUuid);
+    console.log(promotion);
+    if (promotionUuid) {
+      return res.json(promotion);
+    }
+    res.json(await promotionService.getPromotion(req.promotion));
+  } catch (error) {
+    logger.error(`${error}`);
+    return next(boom.badImplementation(error.message));
+  }
+};
 
-//   const userData = req.body;
-//   let response;
+const createPromotion = async (req, res, next) => {
+  const promotionData = req.body;
 
-//   try {
-//     const userUuid = user.uuid;
-//     delete userData.uuid;
-//     response = await userService.putUser(userUuid, userData);
-//   } catch (error) {
-//     if (error.code === 11000 && error.keyPattern) {
-//       const dupField = Object.keys(error.keyValue)[0];
-//       return next(boom.badData(`Ya existe un usuario con ese ${dupField} introducido`));
-//     }
-//     logger.error(`${error}`);
-//     return next(boom.badData(error.message));
-//   }
+  let promotion;
+  try {
+    promotion = await promotionService.createClient(promotionData);
+  } catch (error) {
+    if (error instanceof UniqueConstraintError) {
+      return next(boom.badData('Ya existe esta promoción'));
+    }
+    logger.error(`${error}`);
+    return next(boom.badData(error.message));
+  }
+  /* try {
+    await activityService.createActivity({
+      action: activityActions.CREATE_CLIENT,
+      author: req.user.email,
+      elementBefore: JSON.stringify({}),
+      elementAfter: JSON.stringify(client.toJSON()),
+    });
+  } catch (error) {
+    logger.error(`${error}`);
+  } */
 
-//   try {
-//     await activityService.createActivity({
-//       action: activityActions.UPDATE_USER,
-//       author: req.user.email,
-//       elementBefore: JSON.stringify(user.toJSON()),
-//       elementAfter: JSON.stringify(response.toJSON()),
-//     });
-//   } catch (error) {
-//     logger.error(`${error}`);
-//   }
+  res.status(201).json(promotionService.toPublic(promotion));
+};
 
-//   res.json(userService.toPublic(response));
-// };
+const putPromotion = async (req, res, next) => {
+  let { promotion } = req;
+  if (res.locals && res.locals.promotion) {
+    // eslint-disable-next-line prefer-destructuring
+    promotion = res.locals.promotion;
+  }
 
-// const deleteUser = async (req, res, next) => {
-//   const { user } = res.locals;
-//   const userBeforeDelete = cloneDeep(user);
+  const promotionData = req.body;
+  let response;
 
-//   try {
-//     await userService.deleteUser(user, req.user._id);
-//   } catch (error) {
-//     logger.error(`${error}`);
-//     return next(boom.badImplementation(error.message));
-//   }
+  try {
+    const promotionUuid = promotion.uuid;
+    delete promotionData.uuid;
+    response = await promotionService.putPromotion(promotionUuid, promotionData);
+  } catch (error) {
+    if (error instanceof UniqueConstraintError) {
+      return next(boom.badData('Ya existe esta promoción'));
+    }
+    logger.error(`${error}`);
+    return next(boom.badData(error.message));
+  }
 
-//   try {
-//     await activityService.createActivity({
-//       action: activityActions.DELETE_USER,
-//       author: req.user.toJSON(),
-//       elementBefore: userBeforeDelete.toJSON(),
-//     });
-//   } catch (error) {
-//     logger.error(`${error}`);
-//   }
+  try {
+    await activityService.createActivity({
+      action: activityActions.UPDATE_PROMOTION,
+      author: req.promotion.name,
+      elementBefore: JSON.stringify(promotion.toJSON()),
+      elementAfter: JSON.stringify(response.toJSON()),
+    });
+  } catch (error) {
+    logger.error(`${error}`);
+  }
 
-//   res.status(204).json({});
-// };
+  res.json(promotionService.toPublic(response));
+};
+
+const deletePromotion = async (req, res, next) => {
+  const { promotion } = res.locals;
+  const promotionBeforeDelete = cloneDeep(promotion);
+
+  try {
+    await promotionService.deleteClient(promotion, req.client._id);
+  } catch (error) {
+    logger.error(`${error}`);
+    return next(boom.badImplementation(error.message));
+  }
+
+  try {
+    await activityService.createActivity({
+      action: activityActions.DELETE_PROMOTION,
+      author: req.promotion.toJSON(),
+      elementBefore: promotionBeforeDelete.toJSON(),
+    });
+  } catch (error) {
+    logger.error(`${error}`);
+  }
+
+  res.status(200).json('La promoción ha sido borrada correctamente');
+};
 
 module.exports = {
-  // activate,
-  // login,
-  // register,
-  // forgot,
-  // recovery,
-  // listUsers,
-  // getUser,
+  listPromotions,
+  getPromotion,
   createPromotion,
-  // putUser,
-  // deleteUser,
-  // createMongoUser,
-  // unlockAccount,
+  putPromotion,
+  deletePromotion,
 };
