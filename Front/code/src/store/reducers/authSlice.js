a5e603178ebeb2d7d003fa9daf4ffb9060cd2098
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-02-15 09:38:16 +0000

    WIP: working in login reducer

---
+++ Front/code/src/store/reducers/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+const authSlice = createSlice({
+  name: 'user',
+  initialState: user ? { value: user } : { value: null },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+    logout: (state) => {
+      state.value = false;
+    }
+  }
+});
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users');
+  return response.data;
+})
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
+
+// Selector functions
+
+export const selectUser = (state) => state.user;
