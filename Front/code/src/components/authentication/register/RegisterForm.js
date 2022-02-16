b24f7192b37cca8e380ff6ed3c41f9f9cdc24626
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-02-16 16:16:04 +0000

    FIX: check if user email is included in password

--- Front/code/src/components/authentication/register/RegisterForm.js
+++ Front/code/src/components/authentication/register/RegisterForm.js
@@ -10,6 +10,7 @@ import { useSelector, useDispatch } from 'react-redux';
 // material
 import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import validatePassword from '../../../services/passwordValidator';
 import { setMessage, clearMessage } from '../../../store/reducers/messageSlice';
 import authService from '../../../services/authenticationService';
 
@@ -27,49 +28,7 @@ export default function RegisterForm() {
     password: Yup.string().test({
       password: 'validator-custom-password',
       // eslint-disable-next-line object-shorthand
-      test: function validatePassword(password) {
-        const errors = [];
-
-        if (!password) {
-          return this.createError({
-            message: `Password is required`,
-            path: `password`
-          });
-        }
-
-        if (password.length < 9) {
-          errors.push('Must contain 9 characters');
-        }
-
-        const lowercase = new RegExp(/^(?=.*[a-z]).{1,}$/);
-        if (!lowercase.test(password)) {
-          errors.push('One lowercase');
-        }
-
-        const uppercase = new RegExp(/^(?=.*[A-Z]).{1,}$/);
-        if (!uppercase.test(password)) {
-          errors.push('One uppercase');
-        }
-
-        const number = new RegExp(/^(?=.*[0-9]).{1,}$/);
-        if (!number.test(password)) {
-          errors.push('One number');
-        }
-
-        // eslint-disable-next-line no-useless-escape
-        const symbols = new RegExp(/^(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]).{1,}$/);
-        if (!symbols.test(password)) {
-          const chars = '-;!$%^&*()_+|~=`{}[]:"\'<>?,./';
-          errors.push(`One Special Case Character of ${chars}`);
-        }
-
-        return errors.length > 0
-          ? this.createError({
-              message: `${errors.join(', ')}`,
-              path: `password`
-            })
-          : true;
-      }
+      test: validatePassword
     })
   });
 
