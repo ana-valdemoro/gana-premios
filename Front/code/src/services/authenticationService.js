fc08ccd6a9188226d5363f7cd84f9fc65349995b
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-02-23 11:39:27 +0000

    WIP: add application language in requests without authentication

--- Front/code/src/services/authenticationService.js
+++ Front/code/src/services/authenticationService.js
@@ -1,20 +1,22 @@
 import config from './const';
+import i18n from '../i18n';
 
 const login = async (userCredentials) =>
   fetch(`${config.API_BASE_URI}${config.API_BASE_PORT}/api/v1/auth/login`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Accept-Language': i18n.resolvedLanguage
     },
     body: JSON.stringify(userCredentials)
   }).then((res) => res.json());
 
-const register = async (user, language) =>
+const register = async (user) =>
   fetch(`${config.API_BASE_URI}${config.API_BASE_PORT}/api/v1/auth/register`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Accept-Language': language
+      'Accept-Language': i18n.resolvedLanguage
     },
     body: JSON.stringify(user)
   }).then((res) => res.json());
