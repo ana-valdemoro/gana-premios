dbcf090377f0f06dd73017b1ad8a954694ff5781
Ana Valdemoro <ana.valdemoro@agiliacenter.com>
2022-03-03 12:41:14 +0000

    FTR: restrict access to application if user is not logged in or hasn't upload LOPD

--- Front/code/src/routes/index.js
+++ Front/code/src/routes/index.js
@@ -1,13 +1,15 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
 import AuthenticationRoutes from './AuthenticationRoutes';
-import MainRoutes from './MainRoutes';
+import { MainRoutes } from './MainRoutes';
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
+  const { isLoggedIn, user } = useSelector((state) => state.auth);
   return useRoutes([
-    MainRoutes,
+    MainRoutes(isLoggedIn, user),
     AuthenticationRoutes,
     { path: '*', element: <Navigate to="/404" replace /> }
   ]);
